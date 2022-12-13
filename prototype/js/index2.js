5a75875773a16ac507ea46523a1a2e9b3e5e2052
Sergei199352 <s.stanislavchuk@rgu.ac.uk>
2022-12-13 14:12:28 +0000

    hello

--- prototype/js/index2.js
+++ prototype/js/index2.js
@@ -104,7 +104,7 @@ $(function(){
         var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
         var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
         var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-        console.log(pageNum)
+        
         
 
         // Display the result in the element with id="demo"
