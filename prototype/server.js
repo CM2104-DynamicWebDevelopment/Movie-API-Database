86e4f1606c81be991241fbdd922aa311552e0078
Sergei199352 <s.stanislavchuk@rgu.ac.uk>
2023-04-01 06:53:53 +0100

    working on login

--- prototype/server.js
+++ prototype/server.js
@@ -1,8 +1,15 @@
 // server.js
 // load the things we need
+const MongoClient = require('mongodb').MongoClient; //npm install mongodb@2.2.32
+const url = "mongodb://localhost:27017/profiles";
 var express = require('express');
 var app = express();
 app.use(express.static('public'))
+const bodyParser = require('body-parser'); //npm install body-parser
+
+
+//this tells express we are using sesssions. These are variables that only belong to one user of the site at a time.
+app.use(session({ secret: 'example' }));
 // set the view engine to ejs
 app.set('view engine', 'ejs');
 // use res.render to load up an ejs view file
@@ -11,10 +18,23 @@ app.set('view engine', 'ejs');
 //  res.render('pages/index');
 // });
 
+//variable to hold our Database
+var db;
+
+//this is our connection to the mongo db, ts sets the variable db as our database
+MongoClient.connect(url, function(err, database) {
+  if (err) throw err;
+  db = database;
+  app.listen(8080);
+  console.log('listening on 8080');
+});
+
 app.get('/index2', function(req,res){
+    if(!req.session.loggedin){res.redirect('/login');return;}
     res.render('pages/index2')
 });
 app.get('/trending', function(req,res){
+    if(!req.session.loggedin){res.redirect('/login');return;}
     res.render('pages/trending')
 });
 
@@ -25,5 +45,37 @@ app.get('/', function(req, res) {
 app.get('/index', function(req,res){
     res.render('pages/index')
 });
-app.listen(8080);
-console.log('8080 is the magic port');
+
+app.get('/login', function(req,res){
+
+res.render('pages/login')
+});
+//********** POST ROUTES - Deal with processing data from forms ***************************
+
+
+//the dologin route detasl with the data from the login screen.
+//the post variables, username and password ceom from the form on the login page.
+app.post('/dologin', function(req, res) {
+    console.log(JSON.stringify(req.body))
+    var uname = req.body.username;
+    var pword = req.body.password;
+    req.session.currentuser = uname;
+  
+  
+  
+    db.collection('people').findOne({"login.username":uname}, function(err, result) {
+      if (err) throw err;
+  
+  
+      if(!result){res.redirect('/login');return}
+  
+  
+  
+      if(result.login.password == pword){ req.session.loggedin = true; res.redirect('/') }
+  
+  
+  
+      else{res.redirect('/login')}
+    });
+    
+  });
