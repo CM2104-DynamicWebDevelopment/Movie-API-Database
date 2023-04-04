ce2e426f923d6bf18eb97279cd0aa1dd3b30a9ce
error533 <you@example.com>
2023-04-04 17:01:14 +0100

    fixed

--- prototype/server.js
+++ prototype/server.js
@@ -146,14 +146,3 @@ app.post('/dologin', function(req, res) {
   }
   
 
-
-
-  function onChange() {
-    const password = document.querySelector('input[name=password]');
-    const confirm = document.querySelector('input[name=confirm]');
-    if (confirm.value === password.value) {
-      confirm.setCustomValidity('');
-    } else {
-      confirm.setCustomValidity('Passwords do not match');
-    }
-  }
