ceaa7ae2f87ff876856721d708e399f20914e261
error533 <you@example.com>
2023-04-04 16:26:26 +0100

    sd

--- prototype/server.js
+++ prototype/server.js
@@ -146,13 +146,9 @@ app.post('/dologin', function(req, res) {
   }
   
 
-  var check = function() {
-    if (document.getElementById('password').value ==
-      document.getElementById('confirm_password').value) {
-      document.getElementById('message').style.color = 'green';
-      document.getElementById('message').innerHTML = 'matching';
-    } else {
-      document.getElementById('message').style.color = 'red';
-      document.getElementById('message').innerHTML = 'not matching';
-    }
-  }
+  $('#password, #confirm_password').on('keyup', function () {
+    if ($('#password').val() == $('#confirm_password').val()) {
+      $('#message').html('Matching').css('color', 'green');
+    } else 
+      $('#message').html('Not Matching').css('color', 'red');
+  });
