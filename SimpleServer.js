3aa77319e90c188b6c4b0f153fde5c4a1503dc78
error533 <you@example.com>
2023-04-04 16:23:43 +0100

    sd

--- SimpleServer.js
+++ SimpleServer.js
@@ -70,12 +70,3 @@ http.createServer(function (req, res) {
 console.log(`Simple Server is listening for requests on port ${port}`);
 
 
-function onChange() {
-  const password = document.querySelector('input[name=password]');
-  const confirm = document.querySelector('input[name=confirm]');
-  if (confirm.value === password.value) {
-    confirm.setCustomValidity('');
-  } else {
-    confirm.setCustomValidity('Passwords do not match');
-  }
-}
