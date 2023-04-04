e88899329c3083600627d1ad90be17aa4079a741
error533 <you@example.com>
2023-04-04 16:07:05 +0100

    sd

--- prototype/server.js
+++ prototype/server.js
@@ -145,16 +145,4 @@ app.post('/dologin', function(req, res) {
     console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
   }
 
-  var password = document.getElementById("password")
-  , confirm_password = document.getElementById("confirm_password");
-
-function validatePassword(){
-  if(password.value != confirm_password.value) {
-    confirm_password.setCustomValidity("Passwords Don't Match");
-  } else {
-    confirm_password.setCustomValidity('');
-  }
-}
-
-password.onchange = validatePassword;
-confirm_password.onkeyup = validatePassword;
+  
