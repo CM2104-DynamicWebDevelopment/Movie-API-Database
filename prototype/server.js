506fcfdb17987d7353d437c0f956e4ae77351bd0
Sergei199352 <s.stanislavchuk@rgu.ac.uk>
2023-04-06 16:10:39 +0100

    s

--- prototype/server.js
+++ prototype/server.js
@@ -128,7 +128,7 @@ app.post('/dologin', function(req, res) {
     });
     
   });
-  app.post('/adduser', function(req, res) {
+  app.post('/add', function(req, res) {
     //check we are logged in
     if(!req.session.loggedin){res.redirect('/login');return;}
   
