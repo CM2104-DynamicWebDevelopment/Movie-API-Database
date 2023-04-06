e4f5228d526b8e314193b0896df61a24425a0617
Sergei199352 <s.stanislavchuk@rgu.ac.uk>
2023-04-06 15:56:44 +0100

    form

--- prototype/server.js
+++ prototype/server.js
@@ -128,13 +128,31 @@ app.post('/dologin', function(req, res) {
     });
     
   });
-  app.post('/adduser', function (req, res) {
-    db.collection('quotes').save(req.body, function(err, result) {
+  app.post('/adduser', function(req, res) {
+    //check we are logged in
+    if(!req.session.loggedin){res.redirect('/login');return;}
+  
+    //we create the data string from the form components that have been passed in
+  
+  var datatostore = {
+  "gender":req.body.gender,
+  "name":{"title":req.body.title,"first":req.body.first,"last":req.body.last},
+  "location":{"street":req.body.street,"city":req.body.city,"state":req.body.state,"postcode":req.body.postcode},
+  "email":req.body.email,
+  "login":{"username":req.body.username,"password":req.body.password},
+  "dob":req.body.dob,"registered":Date(),
+  "picture":{"large":"req.body.large","medium":"req.body.medium","thumbnail":"req.body.thumbnail"},
+  "nat":req.body.nat}
+  
+  
+  //once created we just run the data string against the database and all our new data will be saved/
+    db.collection('people').save(datatostore, function(err, result) {
       if (err) throw err;
       console.log('saved to database')
+      //when complete redirect to the index
       res.redirect('/')
     })
-  })
+  });
 
 
   function onSignIn(googleUser) {
