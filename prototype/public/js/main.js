4f3ae0fa29320fbbe9afb047633a5112dbe49dd8
Sergei199352 <s.stanislavchuk@rgu.ac.uk>
2023-04-08 13:12:07 +0100

    s

--- prototype/public/js/main.js
+++ prototype/public/js/main.js
@@ -50,7 +50,7 @@ $(function(){
         
 
         var hString = "";
-        for (var i=0; i<jsondata.total_results; i++){
+        for (var i=0; i<10; i++){
             
             var title = jsondata.Search[i].Title;
             
